89edefe8dc6358df20cb93c64c4cd028fa8ec6dd
Yugo <yu1931sn@ezweb.ne.jp>
2020-01-29 14:19:34 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '09b989dc14a44db70da0e063b57c1fc7fa8c46a47d1a99b07e26666c3cc067f27877c63b9443ee80e6a0e51c00aed345e6a95739432f98b6fcca9d2df80c9909'
